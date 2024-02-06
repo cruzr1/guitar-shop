c31b0bdd924d26af09436da1288b4c1421a05314
Rinat <r.z.zaripov@gmail.com>
2024-02-06 16:05:20 +0300

    feat(project): added nest backend

---
+++ apps/backend/webpack.config.js
@@ -0,0 +1,24 @@
+const { NxWebpackPlugin } = require('@nx/webpack');
+const { join } = require('path');
+
+module.exports = {
+  output: {
+    path: join(__dirname, '../../dist/apps/backend'),
+  },
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  plugins: [
+    new NxWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+    }),
+  ],
+};
