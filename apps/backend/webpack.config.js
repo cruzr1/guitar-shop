18940ff4931258e4ac15b7429a1422ca5e7e88d7
Rinat <r.z.zaripov@gmail.com>
2024-02-07 10:24:22 +0300

    feat(project): исправил настройки webpack.config.ts

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -1,24 +1,17 @@
-const { NxWebpackPlugin } = require('@nx/webpack');
-const { join } = require('path');
+const { NxWebpackPlugin, composePlugins, withNx } = require('@nx/webpack');
 
-module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/backend'),
-  },
-  devServer: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
-  plugins: [
-    new NxWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-    }),
-  ],
-};
+// Nx plugins for webpack.
+module.exports = composePlugins(withNx(), (config) => {
+  // Update the webpack config as needed here.
+  // e.g. `config.plugins.push(new MyPlugin())`
+  config.plugins?.push(new NxWebpackPlugin({
+    target: 'node',
+    compiler: 'tsc',
+    main: './src/main.ts',
+    tsConfig: './tsconfig.app.json',
+    assets: ['./src/assets'],
+    optimization: false,
+    outputHashing: 'none',
+  }))
+  return config;
+});
