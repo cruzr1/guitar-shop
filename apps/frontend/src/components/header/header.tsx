0053ce7940df05a3214b20c7a74322c3fe1fbc6a
Rinat <r.z.zaripov@gmail.com>
2024-02-08 11:18:56 +0300

    fix(frontend): исправил вид меню при авторизации

--- apps/frontend/src/components/header/header.tsx
+++ apps/frontend/src/components/header/header.tsx
@@ -8,10 +8,6 @@ export default function Header ():JSX.Element {
       <div className="container">
         <div className="header__wrapper"><a className="header__logo logo" href="main.html"><img className="logo__img" width="70" height="70" src="./img/svg/logo.svg" alt="Логотип"/></a>
           <NavigationList isAuthorised={isAuthorised}/>
-          <div className="header__container"><span className="header__user-name">Имя</span><a className="header__link" href="login.html" aria-label="Перейти в личный кабинет">
-              <svg className="header__link-icon" width="12" height="14" aria-hidden="true">
-                <use xlinkHref="#icon-account"></use>
-              </svg><span className="header__link-text">Вход</span></a></div>
         </div>
       </div>
     </header>
--- apps/frontend/src/components/navigation-list/navigation-list.tsx
+++ apps/frontend/src/components/navigation-list/navigation-list.tsx
@@ -4,24 +4,35 @@ type NavigationListProps = {
 
 export default function NavigationList ({isAuthorised}: NavigationListProps): JSX.Element {
   return (
-    <nav className="main-nav">
-      <ul className="main-nav__list">
-        {isAuthorised ?
-          <>
-            <li className="main-nav__item"><a className="link main-nav__link" href="#">Каталог</a>
-            </li>
-            <li className="main-nav__item"><a className="link main-nav__link" href="#">Где купить?</a>
-            </li>
-            <li className="main-nav__item"><a className="link main-nav__link" href="#">О компании</a>
-            </li> </>:
+    <>
+      <nav className="main-nav">
+        <ul className="main-nav__list">
+          {!isAuthorised ?
             <>
-            <li className="main-nav__item"><a className="link main-nav__link" href="main">Каталог</a>
-            </li>
-            <li className="main-nav__item"><a className="link main-nav__link" href="#">Список товаров</a>
-            </li>
-          </>
-        }
-      </ul>
-    </nav>
+              <li className="main-nav__item"><a className="link main-nav__link" href="#">Каталог</a>
+              </li>
+              <li className="main-nav__item"><a className="link main-nav__link" href="#">Где купить?</a>
+              </li>
+              <li className="main-nav__item"><a className="link main-nav__link" href="#">О компании</a>
+              </li> </>:
+              <>
+              <li className="main-nav__item"><a className="link main-nav__link" href="main">Каталог</a>
+              </li>
+              <li className="main-nav__item"><a className="link main-nav__link" href="#">Список товаров</a>
+              </li>
+            </>
+          }
+        </ul>
+      </nav>
+      <div className="header__container">
+        <span className="header__user-name">Имя</span>
+        <a className="header__link" href="login.html" aria-label="Перейти в личный кабинет">
+          <svg className="header__link-icon" width="12" height="14" aria-hidden="true">
+            <use xlinkHref="#icon-account"></use>
+          </svg>
+          <span className="header__link-text">Вход</span>
+        </a>
+      </div>
+    </>
   )
 }
