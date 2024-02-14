7807ef620229ec41656a10a9fc100336472959b0
Rinat <r.z.zaripov@gmail.com>
2024-02-14 13:06:43 +0300

    fix(project): исправил метод генерации артикула гитары

--- apps/backend/src/cli/helpers/create-guitar.ts
+++ apps/backend/src/cli/helpers/create-guitar.ts
@@ -3,16 +3,18 @@ import { GuitarCategory, GuitarValidationParams, STRINGS } from '../cli.constant
 import { Guitar, GuitarCategoryType, MockGuitarDataType, StringsCountType } from '@guitar-shop/types';
 import { randomUUID } from 'crypto';
 
+const DIFF = 1;
+
 export function generateGuitar(mockGuitarData: MockGuitarDataType):Guitar {
   const name = getRandomItem<string>(mockGuitarData.names);
   const description = getRandomItem<string>(mockGuitarData.descriptions);
   const imageURL = getRandomItem<string>(mockGuitarData.imageURLs);
   const type = getRandomItem<GuitarCategoryType>(Object.values(GuitarCategory));
   const createdAt = generateRandomDate();
-  const article = randomUUID().slice(
+  const article = randomUUID().slice(0,
     generateRandomValue(
       GuitarValidationParams.Article.Length.Minimum,
-      GuitarValidationParams.Article.Length.Maximum
+      GuitarValidationParams.Article.Length.Maximum - DIFF
     )
   );
   const price = generateRandomValue(
